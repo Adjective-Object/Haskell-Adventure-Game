f1c7c1b0e75158f78937db3ce6299d4bd4f6b1da
Adjective-Object <mhuan13@gmail.com>
2014-07-29 16:44:10 -0400

    some refactoring

--- hello.hs
+++ hello.hs
@@ -10,30 +10,56 @@ module Main where
     unwrapItem :: Item -> String
     unwrapItem (Item s) = s
 
+    data GameState = GameState{
+        items :: [Item],
+        flags :: [Flag]
+    }
+
     -- using flags and items is superior to a pure DFM approach, I think.
     data Room = Room {
         title :: String,
-        description :: [Item] -> [Flag] -> String,
-        links :: [Item] -> [Flag] -> Map String Room
+        description :: GameState -> String,
+        links :: GameState -> Map String (Room,GameState)
         }
 
     room1 = Room {
         title = "A Damp Dungeon",
-        description = (\i f 
-            -> "This is a damp room, because this is an adventure game"),
-        links = (\i f -> fromList [
-            ("THIS ROOM", room1)
+        description = (\g
+            -> "This is a damp dungeon-y starting room, because this is an adventure game"),
+        links = (\g -> fromList [
+            ("THIS ROOM", (room1, g))
         ])
     }
 
-    linkNames :: Room -> [Item] -> [Flag]-> [String]
-    linkNames room items flags = (keys ((links room) items flags))
+    linkNames :: Room -> GameState-> [String]
+    linkNames room gamestate = (keys ((links room) gamestate))
 
-    evalroom :: Room -> [Item] -> [Flag] -> Room
-    evalroom room items flags = let lnks = ((links room) items flags) 
-                                in do
-                                    putStrLn "ugh"
-                                    s <- lnks ! ((keys lnks) !! 0)
-                                    
+    doroombody :: Room -> GameState -> IO ()
+    doroombody room gamestate = 
+        let lnks = ((links room) gamestate)                            
+        in do 
+            putStrLn ("Exits are:"
+                ++ (Prelude.foldr 
+                    (\x y -> y ++ " and " ++ x) 
+                    ""
+                    (keys lnks)))
+            command <- getLine
 
-    main = evalroom room1
+            if command /= ""
+                then putStr("\n")
+                else putStr("")
+
+            if command `elem` (keys lnks) -- backtick makes infix?
+                then let ret = (lnks ! command)
+                    in doroom (fst ret) (snd ret) 
+                else do
+                    putStrLn ("I don't recognize the command \"" ++ command ++ "\"")
+                    doroom room gamestate
+
+    doroom :: Room -> GameState -> IO ()
+    doroom room gamestate = do
+                                putStrLn (title room)
+                                putStrLn ((description room) gamestate)
+                                doroombody room gamestate
+    main :: IO ()
+    main = doroom room1 GameState{items=[], flags=[]}
