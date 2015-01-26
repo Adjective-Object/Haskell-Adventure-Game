cfb19b4afaf6bdadc30acae37a36baabbca62d24
Adjective-Object <mhuan13@gmail.com>
2015-01-26 10:16:54 -0500

    eyy

--- game.hs
+++ game.hs
@@ -3,98 +3,29 @@ module Main where
     import Data.Char as Char
     import Data.Maybe as Maybe
 
-    -- newtype declarations and interfaces
+    import GameDatatypes
+    import RoomDeclarations
 
-    newtype Flag = Flag String deriving (Eq, Show)
-    newtype Item = Item String deriving (Eq, Show)
+    -- Helper Methods
 
-    -- Data Declaraions
+    breakCommand :: String -> (String, String)
+    breakCommand command = let
 
-    data VerbResponse = VerbResponse{
-        message :: String,
-        newstate :: Maybe GameState
-    }
-    -- using flags and items is superior to a pure DFM approach, I think.
-    
-    data GameState = GameState{
-        room :: Room
-        ,items :: [Item]
-        ,flags :: [Flag]
-    }
+                    comlower = (Prelude.map toLower command)
 
-    data Room = Room {
-        title :: String
-        ,description :: GameState -> String
-        ,verbs :: Map String (String -> GameState -> VerbResponse) -- maps VERB strings to change in gamestate
-    }
+                    cmdName :: String
+                    cmdName = (words comlower) !! 0
 
-    -- verb helpers
+                    catString :: String -> String -> String
+                    catString a b = a ++ " " ++ b 
 
-    changeRoom :: GameState -> Room -> GameState
-    changeRoom g r = GameState{
-            room = r,
-            items = items g,
-            flags = flags g
-        } 
+                    in if cmdName == comlower -- easier than installing cabal ,_,
+                        then (cmdName, "")
+                        else (cmdName, init (Prelude.foldr catString "" (drop 1 (words comlower) )) )
 
-    failVerb :: GameState -> String -> VerbResponse
-    failVerb state msg = VerbResponse{message=msg, newstate=Nothing} 
 
-    goRoom :: GameState -> Room -> GameState
-    goRoom instate newroom = GameState{
-                                    flags = flags instate,
-                                    items = items instate,
-                                    room = newroom}
-
-    makeGoList :: [(String, Room)] -> Map String (String -> GameState -> VerbResponse)
-    makeGoList largs = let funcs = fromList([
-                            (fst pair, 
-                                (\ cmdin instate -> 
-                                    goRoom instate (snd pair) 
-                                )
-                            ) | pair <- largs ])
-                        in fromList[ ("go", 
-                            (\ cmdstr gamestate -> 
-                            if cmdstr `elem` keys funcs
-                                then VerbResponse{newstate=Just ((funcs ! cmdstr) cmdstr gamestate), message=""}
-                                else failVerb gamestate ("Cannot go to target \"" ++ cmdstr ++"\".")
-                            )) ]
-
-    addFlag :: GameState -> Flag -> GameState
-    addFlag g f = GameState{
-            room = room g,
-            items = items g,
-            flags = (flags g) ++ [f]
-        }
-
-    -- Room declarations
-
-    room1 = Room {
-        title = "A Damp Dungeon"
-        ,description = (\g
-            -> "This is a damp dungeon-y starting room, \
-                \because this is an adventure game, \
-                \and that is the sort of thing adventure games do. "
-                ++ if (Flag "starting_lever") `elem` (flags g)
-                    then "The lever you pulled is still in \
-                        \the middle of the floor."
-                    else "There is a lever in the middle of the floor, \
-                        \beckoning you to pull it."
-                    
-        )
-        ,verbs = union
-            (makeGoList [
-                ("this room", room1) -- shorthand that makes custom go command
-            ])
-
-            (fromList [
-                ("pull", (\ com state -> 
-                    if com == "lever"
-                        then VerbResponse{newstate=Just (addFlag state (Flag "starting_lever")), message="you pull the lever."}
-                        else failVerb state ("cannot pull \"" ++ com ++ "\".")
-                    ))
-            ])
-    }
+    stripArticles :: String -> String
+    stripArticles command = command
 
     -- Game logic
 
@@ -110,16 +41,7 @@ module Main where
                 vrbs :: Map String (String -> GameState -> VerbResponse) 
                 vrbs = (verbs (room gamestate))
                 
-                comlower = (Prelude.map toLower command)
-
-                cmdName :: String
-                cmdName = (words comlower) !! 0
-
-                catString :: String -> String -> String
-                catString a b = a ++ " " ++ b 
-
-                cmdBody :: String
-                cmdBody = init (Prelude.foldr catString "" (drop 1 (words comlower) ))
+                (cmdName, cmdBody) = breakCommand (stripArticles(command))
 
                 in 
                 if cmdName `elem` (keys vrbs) -- backtick makes infix?
